55ea421fa65f9095f586bf8ed02a145be54bbeda
lsellens <lsellens@gmail.com>
2016-01-14 12:33:03 -0600

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+script.service.rpi.tv.switch
+===================
+
+Used to shut down a Raspberry pi by GPIO 
---
+++ script.service.rpi.tv.switch/service.py
@@ -0,0 +1,29 @@
+import xbmc
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError, e:
+    xbmc.log("rpi.tv.switch: RPi.GPIO import error", level=xbmc.LOGERROR)
+    xbmc.log(str(e), level=xbmc.LOGERROR)
+
+# addon
+__scriptname__   = "rpi tv switch"
+__author__       = "lsellens"
+__url__          = "http://lsellens.openelec.tv"
+
+
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    rxFromSwitch = 18
+    txToSwitch = 16
+    GPIO.setup(rxFromSwitch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(txToSwitch, GPIO.OUT, initial=GPIO.HIGH)
+    monitor = xbmc.Monitor()
+ 
+    while not monitor.abortRequested():
+        if not GPIO.input(rxFromSwitch):
+            xbmc.log("rpi.tv.switch: TV is off. Shutting down RPi", level=xbmc.LOGDEBUG)
+            xbmc.shutdown()
+        if monitor.waitForAbort(10):
+            break
+
