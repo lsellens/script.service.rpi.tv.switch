16514d96a070832cde70bcb26188d362240f9cc0
lsellens <lsellens@gmail.com>
2017-08-07 20:49:42 -0500

    make gpio pins configurable(uses board numbering not bcm)

--- script.service.rpi.tv.switch/service.py
+++ script.service.rpi.tv.switch/service.py
@@ -1,4 +1,5 @@
 import xbmc
+import xbmcaddon
 try:
     import RPi.GPIO as GPIO
 except RuntimeError, e:
@@ -6,16 +7,17 @@ except RuntimeError, e:
     xbmc.log(str(e), level=xbmc.LOGERROR)
 
 # addon
+__addon__ = xbmcaddon.Addon(id='script.service.rpi.tv.switch')
 __scriptname__   = "rpi.tv.switch"
 __author__       = "lsellens"
 __url__          = "https://github.com/lsellens/xbmc.addons"
+txToSwitch = __addon__.getSetting('txToSwitch')
+rxFromSwitch = __addon__.getSetting('rxFromSwitch')
 
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
-    rxFromSwitch = 18
-    txToSwitch = 16
     GPIO.setup(rxFromSwitch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(txToSwitch, GPIO.OUT, initial=GPIO.HIGH)
     monitor = xbmc.Monitor()
